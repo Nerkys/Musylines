13017cf1c1530a9162477b3c3704990da7dfa625
Nerkys <nerthard@gmail.com>
2021-10-23 22:56:35 +0300

    App created

---
+++ Musylines/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Musylines
+//
+//  Created by Mikhail Chukhvantsev on 23.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
